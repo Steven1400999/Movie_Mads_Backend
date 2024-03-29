0ee39ce7bc3781181460847fe1382008490ca024
Steven <quezadasteven570@gmail.com>
2024-03-28 23:46:25 -0600

    migrations, models, seederes, factories, controllers vacios

---
+++ app/Http/Controllers/DubbingController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Dubbing;
+use Illuminate\Http\Request;
+
+class DubbingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Dubbing $dubbing)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Dubbing $dubbing)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Dubbing $dubbing)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Dubbing $dubbing)
+    {
+        //
+    }
+}
